57a2862b689b3c6541eaee74d3482e84ad3fd686
OlaOst <olaa81@gmail.com>
2023-12-06 14:05:54 +0100

    Function app schedules Todos to database

--- FunctionApp/Program.cs
+++ FunctionApp/Program.cs
@@ -1,12 +1,17 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using DotNetCoreSqlDb.Data;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(services => {
+    .ConfigureServices((context, services) => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddDbContext<MyDatabaseContext>(options =>
+            options.UseSqlServer(context.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
     })
     .Build();
 
--- FunctionApp/TestTimerTrigger.cs
+++ FunctionApp/TestTimerTrigger.cs
@@ -1,20 +1,24 @@
 using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using DotNetCoreSqlDb.Data;
+using DotNetCoreSqlDb.Models;
 
 namespace test
 {
     public class TestTimerTrigger
     {
         private readonly ILogger _logger;
+        private readonly MyDatabaseContext _context;
 
-        public TestTimerTrigger(ILoggerFactory loggerFactory)
+        public TestTimerTrigger(ILoggerFactory loggerFactory, MyDatabaseContext context)
         {
             _logger = loggerFactory.CreateLogger<TestTimerTrigger>();
+            _context = context;
         }
 
         [Function("TestTimerTrigger")]
-        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        public async Task Run([TimerTrigger("0 */5 * * * *", RunOnStartup=true)] TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             
@@ -22,6 +26,9 @@ namespace test
             {
                 _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
             }
+
+            _context.Add(new Todo() { Description = "functionapp funker", CreatedDate = DateTime.UtcNow });
+            await _context.SaveChangesAsync();
         }
     }
 }
